e31c25eb10fb39e953097f0897ed86aeef5d5e8d
K1nGsmaN-hub <15162342h@gmail.com>
2021-08-02 00:32:49 +0800

    hash function v2

--- hash-table.js
+++ hash-table.js
@@ -1,8 +1,9 @@
 class HashFunction {
     constructor(size = 10) {
         this.itemCount = 0
-        this.table = Array(size);
+        this.table = Array(size)
         this.keySets = {}
+        this.emptyIndexes = []
     }
 
     _updateItemCount = () => {
@@ -17,24 +18,53 @@ class HashFunction {
         }
     }
 
+    _hasItem = (key) => this.keySets[key]
+
+    _saveItem = (key, value, index = this.itemCount) => {
+        this.keySets[key] = index
+        this.table[index] = value
+    }
+
     setItem = (key, value) => {
         this._checkForOverflow()
 
-        if (this.keySets[key]) {
+        if (this._hasItem(key)) {
             return new Error('This key was used!')
         }
 
-        this.keySets[key] = this.itemCount
-        this.table[this.itemCount] = value
+        if (this.emptyIndexes.length > 0) {
+            const index = this.emptyIndexes[0]
+            this.emptyIndexes.splice(0, 1)
 
-        this._updateItemCount()
+            this._saveItem(key, value, index)
+        } else {
+            this._saveItem(key, value)
+
+            this._updateItemCount()
+        }
+    }
+
+    removeItem = (key) => {
+        if (!this._hasItem(key)) {
+            return new Error('This key is never used!')
+        }
+
+        const index = this.getIndex(key)
+
+        delete this.keySets[key]
+        this.table[index] = undefined
+        this.emptyIndexes.push(index)
+    }
+
+    updateItem = (key, value) => {
+        if (!this._hasItem(key)) {
+            return new Error('This key is never used!')
+        }
+
+        this.table[this.getIndex(key)] = value
     }
 
     getIndex = (key) => this.keySets[key]
 
     getItem = (key) => this.table[this.getIndex(key)]
-
-    getItemByIndex = (index) => this.table[index]
 }
-
-const hashTable = new HashFunction(10)
